c7e09a5a24760e76f79da06323595ca156351f09
Utilisateur Lambda <clement.chassem@gmail.com>
2024-11-11 13:41:20 +0100

    Add P0T8

---
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,13 @@
+function getCurrentYear() {
+    const date = new Date();
+    return date.getFullYear();
+  }
+  
+  export default function getBudgetForCurrentYear(income, gdp, capita) {
+    // supprime la variable budget et retourne directement l'objet
+    return {
+        [`income-${getCurrentYear()}`]: income,
+        [`gdp-${getCurrentYear()}`]: gdp,
+        [`capita-${getCurrentYear()}`]: capita,
+    };
+  }
