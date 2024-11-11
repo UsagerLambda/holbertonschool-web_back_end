0fd8132f5ad9712a351b965ddd81d918a7a29bed
Utilisateur Lambda <clement.chassem@gmail.com>
2024-11-11 16:22:29 +0100

    Fix ES6_basics style

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,10 +1,10 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const list = []
-    // pour value dans array
-    for (let value of array) {
-        // écrit dans "list" le résultat de appendString + value
-      list.push(appendString + value);
-    }
-    // retourne la list
-    return list;
+  const list = [];
+  // pour value dans array
+  for (let value of array) {
+    // écrit dans "list" le résultat de appendString + value
+    list.push(appendString + value);
   }
+  // retourne la list
+  return list;
+}
--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,6 +1,5 @@
 export default function returnHowManyArguments(...args) {
-    // "...args" récupère les arguments dans un tableau "args"
-    // puis renvoie le nombre d'éléments dans le tableau
-    return args.length;
-
+// "...args" récupère les arguments dans un tableau "args"
+// puis renvoie le nombre d'éléments dans le tableau
+  return args.length;
 }
