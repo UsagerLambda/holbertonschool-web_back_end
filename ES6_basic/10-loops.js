dc1ca4a308e76915266b68fd32fb9decc5ef04ee
Utilisateur Lambda <clement.chassem@gmail.com>
2024-11-11 14:36:51 +0100

    Fix P0T10

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,9 +1,10 @@
 export default function appendToEachArrayValue(array, appendString) {
+    const list = []
     // pour value dans array
     for (let value of array) {
-        // value égal "correctly-"" + "value" (attache les deux strings (correctly-value))
-      value = appendString + value;
+        // écrit dans "list" le résultat de appendString + value
+      list.push(appendString + value);
     }
-    // retourne la liste modifiée
-    return array;
+    // retourne la list
+    return list;
   }
