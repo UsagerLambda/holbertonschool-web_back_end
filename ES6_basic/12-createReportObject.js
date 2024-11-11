9ea17c4c6e7faa306d819933d2d4aa9714b4cf11
Utilisateur Lambda <clement.chassem@gmail.com>
2024-11-11 15:55:42 +0100

    Add P0T12

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,11 @@
+export default function createReportObject(employeesList) {
+    // le code renvoie la liste des objets de employeesList & le nombre de clés
+    return {
+        // all Employees stocke les objets stocké dans employeesList
+        allEmployees: {...employeesList,},
+        getNumberOfDepartments() {
+            // retourne le nombre de key dans employeesList
+            return Object.keys(employeesList).length;
+          },
+    };
+}
