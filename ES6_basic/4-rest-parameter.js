63097c7ea63e8623458bb86e781c428200850ba3
Utilisateur Lambda <clement.chassem@gmail.com>
2024-11-11 11:39:28 +0100

    Add P0T4

---
+++ ES6_basic/4-rest-parameter.js
@@ -0,0 +1,6 @@
+export default function returnHowManyArguments(...args) {
+    // "...args" récupère les arguments dans un tableau "args"
+    // puis renvoie le nombre d'éléments dans le tableau
+    return args.length;
+
+}
