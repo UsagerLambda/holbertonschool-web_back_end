e80b2516bc373b4e9dbaf7401c5fa7e5750dfbff
Utilisateur Lambda <clement.chassem@gmail.com>
2024-11-12 11:29:33 +0100

    Fix P1T9

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -17,35 +17,38 @@ export class HolbertonClass {
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
+// fix
 export class StudentHolberton {
-  constructor(firstName, lastName, holbertonClass) { //fix
+  constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = holbertonClass; //fix
+    this._holbertonClass = holbertonClass;
   }
 
   get fullName() {
     return `${this._firstName} ${this._lastName}`;
   }
 
+  // fix
   get holbertonClass() {
     return this._holbertonClass;
   }
 
+  // fix
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`; //fix
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
-// moved (placé après la création de la class)
+// fix: moved (placé après la création de la class)
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-// crée la liste après avoir défini les étudiants
-const listOfStudents = [student1, student2, student3, student4, student5]; //fix
+// fix: crée la liste après avoir défini les étudiants
+const listOfStudents = [student1, student2, student3, student4, student5];
 
-//exportation par default de la liste
-export default listOfStudents; //fix
+// fix: exportation par default de la liste
+export default listOfStudents;
