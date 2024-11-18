5af500d557d952ecf35500e0174700700a698d7c
Utilisateur Lambda <clement.chassem@gmail.com>
2024-11-18 10:17:32 +0100

    Add P0T4

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,15 @@
+export default function updateStudentGradeByCity(listStudents, city, newGrades) {
+  // pour student.location égal à location dans listStudents
+  return listStudents.filter((student) => student.location === city)
+  .map((student) => {
+    // trouve les newGrades correspondant aux id des étudiants
+    const gradeObj = newGrades.find(grade => grade.studentId === student.id)
+    // si gradeObj n'est pas nULL
+    if (gradeObj) {
+      // retourne student avec le champ grade = gradeObj
+      return { ...student, grade: gradeObj };
+    }
+    // si gradeObj est NULL (donc pas de correspondance grade.studentId avec student.id)
+    return { ...student, grade: 'N/A' };
+  });
+}
