caa350f9127e501ecdb9adfe92752c4321364690
Utilisateur Lambda <clement.chassem@gmail.com>
2024-11-19 11:10:32 +0100

    Fix P1T6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,5 +2,14 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  return Promise.allSettled(promises).then((results) => {
+    // parcourt chaque élément du tableau results
+    return results.map((result) => ({
+      // et pour chaque éléments dans results
+      status: result.status, // on stocke le statut de la promesse
+      // si la promesse est résolue, on garde la valeur de statut, sinon on donne la raison de l'échec
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    }));
+  });
 }
