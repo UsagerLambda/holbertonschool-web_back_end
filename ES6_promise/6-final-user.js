0b45f183a310222434c10c34948c9681b5b94df6
Utilisateur Lambda <clement.chassem@gmail.com>
2024-11-19 11:15:57 +0100

    Fix P1T6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,10 +5,10 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
   return Promise.allSettled(promises).then((results) => {
     // parcourt chaque élément du tableau results
-    return results.map((result) => ({
+    results.map((result) => ({
       // et pour chaque éléments dans results
       status: result.status, // on stocke le statut de la promesse
-      // si la promesse est résolue, on garde la valeur de statut, sinon on donne la raison de l'échec
+      // si la promesse est résolue, on garde la valeur de statut, sinon, raison de l'échec
       value: result.status === 'fulfilled' ? result.value : result.reason,
     }));
   });
