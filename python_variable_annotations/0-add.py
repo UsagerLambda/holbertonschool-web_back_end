d487f1b5f0583a6e0d74eb22f6b3e5c83d218910
Utilisateur Lambda <clement.chassem@gmail.com>
2024-12-02 13:51:39 +0100

    Moving repository from higher level programming

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+# 0-add.py
+
+"""
+This module provides utility functions for mathematical operations.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers.
+
+    Args:
+    a (float): The first number.
+    b (float): The second number.
+
+    Returns:
+    float: The sum of the two numbers.
+    """
+    return a + b
