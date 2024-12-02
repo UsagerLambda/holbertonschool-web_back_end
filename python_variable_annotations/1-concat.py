aa10b505af5af439a6e0fd09e3410bd1581d3b1a
Utilisateur Lambda <clement.chassem@gmail.com>
2024-12-02 13:57:53 +0100

    Add P1T1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+Function that concatenete string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+
+    Args:
+        str1 (str): string 1
+        str2 (str): string 2
+
+    Returns:
+        str: concatened string
+    """
+    return str1 + str2
