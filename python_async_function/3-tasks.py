26ed426aff1727dd68b1863a34fb2242325ef3d6
Utilisateur Lambda <clement.chassem@gmail.com>
2024-12-03 08:44:57 +0100

    Add P2T3

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+""" 3-tasks.py """
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+
+    Args:
+        max_delay (int): argument passed to wait_random
+
+    Returns:
+        asyncio.Task: An asyncio.Task object
+        wrapping the execution of wait_random.
+    """
+    task = asyncio.Task(wait_random(max_delay))
+    return task
