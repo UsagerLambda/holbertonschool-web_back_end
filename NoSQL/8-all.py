33fdc7829c38e079c5390952906910996272bf33
Utilisateur Lambda <clement.chassem@gmail.com>
2024-12-09 16:48:22 +0100

    Fix P2T8

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -6,4 +6,12 @@ return an empty list if no ducuments found in the collection
 
 
 def list_all(mongo_collection):
+    """
+
+    Args:
+        mongo_collection (_type_): collection
+
+    Returns:
+        _type_: list documents in mongo_collection
+    """
     return list(mongo_collection.find())
