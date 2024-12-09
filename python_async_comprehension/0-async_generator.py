7439d17b7e7f22e81f1268c926c665d40fae73a5
Utilisateur Lambda <clement.chassem@gmail.com>
2024-12-09 08:23:25 +0100

    Fix P0T0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -18,6 +18,6 @@ async def async_generator():
     Yields:
         float: A random float number between 0 and 10
     """
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
