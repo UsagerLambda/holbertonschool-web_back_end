e2cd000dbb63a5dc6da0c166c5d530e0cdb04896
Utilisateur Lambda <clement.chassem@gmail.com>
2024-12-09 17:07:23 +0100

    Fix P2T10

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -11,4 +11,4 @@ def update_topics(mongo_collection, name, topics):
         name (_type_): documents to find
         topics (_type_): dict to update
     """
-    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
