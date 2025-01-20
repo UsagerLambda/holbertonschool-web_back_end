b5a6507dde51c1636a37b637a54bc5f3ef0457a4
Utilisateur Lambda <clement.chassem@gmail.com>
2025-01-20 14:49:20 +0100

    Roll back P0 T4 & T5

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,7 +1,7 @@
-const http = require('http');
+const app = require('http');
 
 // create a server object:
-const app = http.createServer((req, res) => {
+app.createServer((req, res) => {
   res.write('Hello Holberton School!'); // write a response to the client
   res.end(); // end the response
 }).listen(1245); // the server object listens on port 1245
