6d7c1a212072786fc01bbb37157067f6afb85d1b
Utilisateur Lambda <clement.chassem@gmail.com>
2025-01-20 14:47:04 +0100

    Fix P0 T4 & T5

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,7 +1,7 @@
-const app = require('http');
+const http = require('http');
 
 // create a server object:
-app.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.write('Hello Holberton School!'); // write a response to the client
   res.end(); // end the response
 }).listen(1245); // the server object listens on port 1245
--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,9 +1,9 @@
-const app = require('http');
+const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const databasePath = process.argv[2] || '';
 
-app.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   if (req.url === '/') {
     res.write('Hello Holberton School!');
   } else if (req.url === '/students') {
