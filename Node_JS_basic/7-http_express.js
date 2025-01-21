6143b49d606df0c8644cd5f167b222e446760053
Utilisateur Lambda <clement.chassem@gmail.com>
2025-01-21 14:08:07 +0100

    Fix T7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -3,40 +3,34 @@ const fs = require('fs').promises;
 
 const app = express();
 
-async function countStudents(path) {
-  try {
-    const data = await fs.readFile(path, 'utf8');
-    const lines = data.split('\n').filter((line) => line.trim() !== '');
+function countStudents(path) {
+  return fs.readFile(path, 'utf8')
+    .then((data) => {
+      const lines = data.trim().split('\n');
+      const students = lines.slice(1).filter((line) => line.length > 0);
 
-    if (lines.length <= 1) {
+      const csStudents = [];
+      const sweStudents = [];
+
+      students.forEach((student) => {
+        const studentData = student.split(',');
+        const field = studentData[3].trim();
+        if (field === 'CS') {
+          csStudents.push(studentData[0].trim());
+        } else if (field === 'SWE') {
+          sweStudents.push(studentData[0].trim());
+        }
+      });
+
+      return [
+        `Number of students: ${students.length}`,
+        `Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`,
+        `Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`,
+      ].join('\n');
+    })
+    .catch(() => {
       throw new Error('Cannot load the database');
-    }
-
-    const students = lines.slice(1);
-
-    const csStudents = [];
-    const sweStudents = [];
-
-    students.forEach((student) => {
-      const studentData = student.split(',');
-      const field = studentData[3].trim();
-      if (field === 'CS') {
-        csStudents.push(studentData[0].trim());
-      } else if (field === 'SWE') {
-        sweStudents.push(studentData[0].trim());
-      }
     });
-
-    return [
-      `Number of students: ${students.length}`,
-      `Number of students in CS: ${
-        csStudents.length}. List: ${csStudents.join(', ')}`,
-      `Number of students in SWE: ${
-        sweStudents.length}. List: ${sweStudents.join(', ')}`,
-    ].join('\n');
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
 }
 
 app.get('/', (req, res) => {
