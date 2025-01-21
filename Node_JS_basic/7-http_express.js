98ddb7b8539d279a1129f0a0f9656d05dcb94865
Utilisateur Lambda <clement.chassem@gmail.com>
2025-01-21 09:18:36 +0100

    Fix P0 T6 & T7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,16 +1,67 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs').promises;
 
-const databasePath = process.argv[2] || '';
 const app = express();
+const databasePath = process.argv[2];
+
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(path, 'utf8');
+
+    const lines = data.trim().split('\n');
+    const students = lines.slice(1).filter((line) => line);
+
+    const csStudents = [];
+    const sweStudents = [];
+
+    students.forEach((student) => {
+      const studentData = student.split(','); // split les données par la virgule
+      const field = studentData[3]; // Colonne field
+      if (field === 'CS') {
+        // ajoute l'étudiant correspondant à la liste
+        csStudents.push(studentData[0]);
+      }
+    });
+
+    students.forEach((student) => {
+      const studentData = student.split(',');
+      const field = studentData[3];
+      if (field === 'SWE') {
+        sweStudents.push(studentData[0]);
+      }
+    });
+
+    return {
+      totalStudents: students.length,
+      csStudents,
+      sweStudents,
+    };
+
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-  res.send('This is the list of our students');
-  countStudents(databasePath);
+  countStudents(databasePath)
+    .then((studentData) => {
+      const output = [
+        'This is the list of our students',
+        `Number of students: ${studentData.totalStudents}`,
+        `Number of students in CS: ${studentData.csStudents.length}. List: ${studentData.csStudents.join(', ')}`,
+        `Number of students in SWE: ${studentData.sweStudents.length}. List: ${studentData.sweStudents.join(', ')}`,
+      ].join('\n');
+      res.send(output);
+    })
+    .catch((error) => {
+      res.status(404).send(error.message);
+    });
 });
 
 app.listen(1245);
+
+module.exports = app;
