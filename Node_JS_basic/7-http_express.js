25e09f0d90e35f05af210585ad3428292fe7ee02
Utilisateur Lambda <clement.chassem@gmail.com>
2025-01-21 11:56:52 +0100

    Fix P0T7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -2,46 +2,46 @@ const express = require('express');
 const fs = require('fs').promises;
 
 const app = express();
-const databasePath = process.argv[2];
 
-async function countStudents(path) {
-  try {
-    const data = await fs.readFile(path, 'utf8');
-    const lines = data.trim().split('\n');
+function countStudents(path) {
+  return fs.readFile(path, 'utf8')
+    .then((data) => {
+      const lines = data.trim().split('\n');
 
-    if (lines.length <= 1) {
-      return 'Number of students: 0';
-    }
-
-    const students = lines.slice(1).filter((line) => line.trim());
-
-    const csStudents = [];
-    const sweStudents = [];
-
-    students.forEach((student) => {
-      const studentData = student.split(',');
-      const field = studentData[3].trim();
-      if (field === 'CS') {
-        csStudents.push(studentData[0].trim());
-      } else if (field === 'SWE') {
-        sweStudents.push(studentData[0].trim());
+      if (lines.length <= 1) {
+        return 'Number of students: 0';
       }
-    });
 
-    return [
-      `Number of students: ${students.length}`,
-      `Number of students in CS: ${
-        csStudents.length}. List: ${csStudents.join(', ')}`,
-      `Number of students in SWE: ${
-        sweStudents.length}. List: ${sweStudents.join(', ')}`,
-    ].join('\n');
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
+      const students = lines.slice(1).filter((line) => line.trim());
+
+      const csStudents = [];
+      const sweStudents = [];
+
+      students.forEach((student) => {
+        const studentData = student.split(',');
+        const field = studentData[3].trim();
+        if (field === 'CS') {
+          csStudents.push(studentData[0].trim());
+        } else if (field === 'SWE') {
+          sweStudents.push(studentData[0].trim());
+        }
+      });
+
+      return [
+        `Number of students: ${students.length}`,
+        `Number of students in CS: ${
+          csStudents.length}. List: ${csStudents.join(', ')}`,
+        `Number of students in SWE: ${
+          sweStudents.length}. List: ${sweStudents.join(', ')}`,
+      ].join('\n');
+    })
+    .catch((error) => {
+      throw new Error('Cannot load the database');
+    });
 }
 
 app.get('/', (req, res) => {
-  res.end('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
